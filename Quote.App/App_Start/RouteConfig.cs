f2c20dd234f2d4f26ecc5459c5a1d6f5a270f74f
Lam Nguyen <nguyenlam3121999@gmail.com>
2021-05-10 11:55:51 +0700

    Set Startup page and Deploy version 1.0

--- Quote.App/App_Start/RouteConfig.cs
+++ Quote.App/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace Quote.App
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Quote", action = "Random", id = UrlParameter.Optional }
             );
         }
     }
